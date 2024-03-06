e83d7b32a20ec9e889040e14e77c1e31c3996b75
rendiputra <rendiputrapradana@gmail.com>
2024-03-07 06:40:50 +0700

    char: add payment provider contraint unique code

--- src/main/resources/db/migration/V20240307__Skema_Awal.sql
+++ src/main/resources/db/migration/V20240307__Skema_Awal.sql
@@ -1,7 +1,11 @@
 CREATE TABLE payment_provider (
     id varchar(36),
+    code varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
     PRIMARY KEY (id)
 );
+
+ALTER TABLE payment_provider ADD CONSTRAINT payment_provider_unique_code UNIQUE (code);
 
 CREATE TABLE invoice_type (
   id varchar(36),
